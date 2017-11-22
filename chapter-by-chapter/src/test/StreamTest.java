88049186c97195471276563d4f5a66b5d35888d9
juligove <j.andres2087@gmail.com>
2017-11-22 16:59:33 -0500

    pruebas reduce

--- chapter-by-chapter/src/test/StreamTest.java
+++ chapter-by-chapter/src/test/StreamTest.java
@@ -1,14 +1,6 @@
 package test;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class StreamTest{
 
-    @Test
-    void myFirstTest() {
-        assertEquals(2, 1 + 1);
-    }
 
 
 }
