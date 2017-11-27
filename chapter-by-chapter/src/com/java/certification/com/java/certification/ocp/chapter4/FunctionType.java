56e4f6378737a0a5c5e1ea2b6f8e6520e8ffb2b4
juligove <j.andres2087@gmail.com>
2017-11-27 15:58:31 -0500

    composing Function Type

--- chapter-by-chapter/src/com/java/certification/com/java/certification/ocp/chapter4/FunctionType.java
+++ chapter-by-chapter/src/com/java/certification/com/java/certification/ocp/chapter4/FunctionType.java
@@ -26,17 +26,28 @@ public class FunctionType {
         // Class::method
         Integer value3 = nameMap.computeIfAbsent("vicente", String::length);
 
+        //Note que efectivamente el lo mismo que enviar la funcion stringSize0
+        Function<String, Integer> stringSize0 = String::length;
+        Integer value4 = nameMap.computeIfAbsent("vicente", stringSize0);
+
         System.out.println("FunctionType:::"+value);
         System.out.println("FunctionType:::"+value2);
         System.out.println("FunctionType:::"+value3);
+        System.out.println("FunctionType:::"+value4);
 
 
 
         //The Function interface has also a default compose method
         //Componer varios functions
 
+        Map<String, String> nameMap2 = new HashMap<>();
+        nameMap2.put("julian", "GONZALEZ");
         Function<String, Integer> stringSize = s -> s.length();
-        Function<Integer, String> stringCompose = s -> String.format("El key es de tamaño %s",s);
-        String compose1 = nameMap.computeIfAbsent("vicente", stringSize.compose(stringCompose));
+        Function<Integer, String> intToStr = s -> String.format("El key es de tamaño %s",s);
+        Function<String, String> composedFunc = intToStr.compose(stringSize);
+        String val5 = nameMap2.computeIfAbsent("vicente", composedFunc);
+        String val6 = nameMap2.computeIfAbsent("julian", composedFunc);
+        System.out.println(val5);
+        System.out.println(val6);
     }
 }
