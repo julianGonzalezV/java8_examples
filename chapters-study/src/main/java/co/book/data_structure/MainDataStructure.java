0a77c6fead616dfde25aa810e05f0575e1f996b5
julianGonzalezV <j.andres2087@gmail.com>
2018-10-11 22:44:02 -0500

    arry example

--- chapters-study/src/main/java/co/book/data_structure/MainDataStructure.java
+++ chapters-study/src/main/java/co/book/data_structure/MainDataStructure.java
@@ -20,7 +20,6 @@ public class MainDataStructure {
          * Cuando lo cree así no puede especificar el tamanio, genera error de sintaxis
          */
         int[] numbers2 = new int[] {99, 55, 42};
-        Arrays.sort(numbers2);
         System.out.println(numbers2[2]);
 
         /**
@@ -39,7 +38,42 @@ public class MainDataStructure {
         Object[] objects = strings;
         String[] againStrings = (String[]) objects;
         //againStrings[0] = new StringBuilder(); // DOES NOT COMPILE, error de complación
-        objects[0] = new StringBuilder();// careful!, genera error en tiempo de ejecución
+        //objects[0] = new StringBuilder();// sea cuidadoso!, genera error en tiempo de ejecución
+        //porque objects es igual a un array de strings, si furea igual a un array de object no
+        //generaría el error
+
+
+        /**
+         *Sorting
+         */
+        Arrays.sort(numbers2);
+        System.out.println("Ordenado es "+numbers2[2]);
+
+
+        /**
+         *Searching
+         * realiza un binary search que requiere un array ordenado ya que divide y luego mira por cual mitad
+         * se va
+         */
+        int[] numbers = {2,4,6,8};
+        System.out.println(Arrays.binarySearch(numbers,2));
+        System.out.println(Arrays.binarySearch(numbers, 4));
+        System.out.println(Arrays.binarySearch(numbers, 1));
+        System.out.println(Arrays.binarySearch(numbers, 3));
+        System.out.println(Arrays.binarySearch(numbers,9)); //-5 PORQUE EL 9 debería ir despues del 3 posicion del array (el 8)
+        //osea en la posición 4 , lo que hace java es negarlo a -4y le resta -1 = -5
+
+        /**
+         * Multidimensional Arrays
+         */
+        int[][] vars1;// 2D array
+        int vars2 [][];// 2D array
+        int[] vars3[];// 2D array
+        int[] vars4 [], space [][]; // a 2D AND a 3D array
+
+        String [][] rectangle = new String[3][2];// es un array de 3 posiciones y cada posición contendrá
+        //otro array de 2  posiciones
+
 
     }
 
