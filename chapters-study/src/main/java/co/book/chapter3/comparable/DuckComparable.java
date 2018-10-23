74e27dc98d1dafa501260540caf830d44844208d
julianGonzalezV <j.andres2087@gmail.com>
2018-10-22 22:37:27 -0500

    comparable I

--- chapters-study/src/main/java/co/book/chapter3/comparable/DuckComparable.java
+++ chapters-study/src/main/java/co/book/chapter3/comparable/DuckComparable.java
@@ -1,4 +1,11 @@
 package co.book.chapter3.comparable;
 
+/**
+ * If your class implements Comparable , it can be used in these data structures that require
+ comparison
+ *The Comparable interface has only one method
+ * public int compareTo(T o): note como aplican generics, evitando así validaciones de tipo
+ * haciendo casting
+ */
 public class DuckComparable {
 }
