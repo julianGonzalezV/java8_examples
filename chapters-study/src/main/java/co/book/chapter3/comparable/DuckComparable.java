674827b470bd35ec2ec801cd3d307b9834920f3b
julianGonzalezV <j.andres2087@gmail.com>
2018-10-23 22:27:45 -0500

    comparable

--- chapters-study/src/main/java/co/book/chapter3/comparable/DuckComparable.java
+++ chapters-study/src/main/java/co/book/chapter3/comparable/DuckComparable.java
@@ -1,5 +1,10 @@
 package co.book.chapter3.comparable;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * If your class implements Comparable , it can be used in these data structures that require
  comparison
@@ -7,5 +12,39 @@ package co.book.chapter3.comparable;
  * public int compareTo(T o): note como aplican generics, evitando así validaciones de tipo
  * haciendo casting
  */
-public class DuckComparable {
+public class DuckComparable implements Comparable<DuckComparable> {
+
+    private String name;
+
+    public DuckComparable(String name){
+        this.name = name;
+    }
+
+    /**
+     * Para que cuando hagamos print del objeto sea legible
+     * @return
+     */
+    public String toString() {
+        return name;
+    }
+
+
+    @Override
+    public int compareTo(DuckComparable o) {
+        //return 0;
+        //si tuviera el atributo Id y entero entonce ssería
+        //return return id – o.id;
+        return name.compareTo(o.name);//si son iguales retorna 0 sino un valor entero positivo cuando el objeto
+        //actual es mas grande que contra el que se compara , o <0 si es menor
+    }
+
+
+
+    public static void main(String[] args) {
+        List<DuckComparable> ducks = new ArrayList<>();
+        ducks.add(new DuckComparable("Quack"));
+        ducks.add(new DuckComparable("Puddles"));
+        Collections.sort(ducks);
+        System.out.println(ducks);// [Puddles, Quack], note como ordena por nombre
+    }
 }
