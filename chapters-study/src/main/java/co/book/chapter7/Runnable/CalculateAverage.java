c89ff5aa2235adfe1c2a29177283c30e8349ef2f
julianGonzalezV <j.andres2087@gmail.com>
2018-12-10 22:22:12 -0500

    runnable

---
+++ chapters-study/src/main/java/co/book/chapter7/Runnable/CalculateAverage.java
@@ -0,0 +1,22 @@
+package co.book.chapter7.Runnable;
+
+
+/**
+ * runnable es una interfaz
+ *
+ * @FunctionalInterface public interface Runnable {
+void run();
+}
+
+ debemos implemenar el run
+ Lo que permite runnable es ejecutar una tarea en un hilo que es definido por el usuario
+
+ */
+public class CalculateAverage implements Runnable {
+
+
+    @Override
+    public void run() {
+        // Define work here
+    }
+}
