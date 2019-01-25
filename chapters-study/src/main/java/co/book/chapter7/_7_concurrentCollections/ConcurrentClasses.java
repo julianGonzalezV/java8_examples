01331798da2e47dfbd34790ed81a0b949f8cb852
julianGonzalezV <j.andres2087@gmail.com>
2019-01-24 22:25:10 -0500

    concurrent collections

---
+++ chapters-study/src/main/java/co/book/chapter7/_7_concurrentCollections/ConcurrentClasses.java
@@ -0,0 +1,42 @@
+package co.book.chapter7._7_concurrentCollections;
+
+import java.util.Deque;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ConcurrentClasses {
+
+    public static void ConcurrentHMap(){
+        Map<String,Integer> map = new ConcurrentHashMap<>();
+        map.put("zebra", 52);
+        map.put("elephant", 10);
+        System.out.println(map.get("elephant"));
+
+    }
+
+    public static void ConcurrentLinkedQ(){
+        Queue<Integer> queue = new ConcurrentLinkedQueue<>();
+        queue.offer(31);
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+    }
+
+    public static void ConcurrentLinkedDeq(){
+        Deque<Integer> deque = new ConcurrentLinkedDeque<>();
+        deque.offer(10);
+        deque.push(4);
+        System.out.println(deque.peek());
+        System.out.println(deque.pop());
+    }
+
+    public static void main(String ... args){
+
+        ConcurrentHMap();
+        ConcurrentLinkedQ();
+        ConcurrentLinkedDeq();
+
+    }
+}
