1b84d89f28a6a9a81f127295cc0b68537b00b11c
julianGonzalezV <j.andres2087@gmail.com>
2019-02-04 16:58:05 -0500

    doc code

--- chapters-study/src/main/java/co/book/chapter7/_7_concurrentCollections/ConcurrentClasses.java
+++ chapters-study/src/main/java/co/book/chapter7/_7_concurrentCollections/ConcurrentClasses.java
@@ -3,9 +3,7 @@ package co.book.chapter7._7_concurrentCollections;
 import java.util.Deque;
 import java.util.Map;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.*;
 
 public class ConcurrentClasses {
 
@@ -33,21 +31,50 @@ public class ConcurrentClasses {
         System.out.println(deque.pop());
     }
 
-
+    /***
+     * Funciona como un Queue normal,  pero contiene métodos que
+     * esperaran cierto tiempo hasta que se complete la operación
+     */
     public static void blockingQueue(){
-        Deque<Integer> deque = new ConcurrentLinkedDeque<>();
-        deque.offer(10);
-        deque.push(4);
-        System.out.println(deque.peek());
-        System.out.println(deque.pop());
+        try {
+            BlockingQueue<Integer> blockingQueue = new LinkedBlockingQueue<>();
+            /*
+                Adds item to the queue waiting the specified time,
+                returning false if time elapses before space is available
+             */
+            blockingQueue.offer(39);
+            blockingQueue.offer(3, 4, TimeUnit.SECONDS);
+            /*
+            Poll:
+                Retrieves and removes an item from the queue, waiting
+                the specified time, returning null if the time elapses
+                before the item is available
+             */
+            System.out.println(blockingQueue.poll());
+            System.out.println(blockingQueue.poll(10, TimeUnit.MILLISECONDS));
+        } catch (InterruptedException e) {
+            System.out.println("blockingQueue => La ejecución fué interrumpida");
+        }
     }
 
+    /**
+     * Misma especificacion de la anterior con la diferencia que Dqueue es
+     * una cola pura
+     */
     public static void blockingDeQueue(){
-        Deque<Integer> deque = new ConcurrentLinkedDeque<>();
-        deque.offer(10);
-        deque.push(4);
-        System.out.println(deque.peek());
-        System.out.println(deque.pop());
+        try {
+            BlockingDeque<Integer> blockingDeque = new LinkedBlockingDeque<>();
+            blockingDeque.offer(91);
+            blockingDeque.offerFirst(5, 2, TimeUnit.MINUTES);
+            blockingDeque.offerLast(47, 100, TimeUnit.MICROSECONDS);
+            blockingDeque.offer(3, 4, TimeUnit.SECONDS);
+            System.out.println(blockingDeque.poll());
+            System.out.println(blockingDeque.poll(950, TimeUnit.MILLISECONDS));
+            System.out.println(blockingDeque.pollFirst(200, TimeUnit.NANOSECONDS));
+            System.out.println(blockingDeque.pollLast(1, TimeUnit.SECONDS));
+        } catch (InterruptedException e) {
+            System.out.println("blockingDDQueue => La ejecución fué interrumpida");
+        }
     }
 
     public static void main(String ... args){
