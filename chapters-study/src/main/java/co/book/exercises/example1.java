f1c616a090c71b3c8dd539981c7db9902eab6b73
julianGonzalezV <j.andres2087@gmail.com>
2019-02-04 15:50:41 -0500

    documentando código

--- chapters-study/src/main/java/co/book/exercises/example1.java
+++ chapters-study/src/main/java/co/book/exercises/example1.java
@@ -3,23 +3,67 @@ package co.book.exercises;
 import java.util.*;
 
 public class example1 {
-    public static void main(String[] args) {
-        List<Integer> inputList = Arrays.asList(10, 2, 10, 7, 5, 2, 9, 7, 8, 11,11);
+
+    /**
+     * Metodo para contar los elementos que están repetidos en la lista de
+     * entrada input
+     * @return
+     */
+    public static int example1(List<Integer> inputList){
+        System.out.println("example1 Input  => "+inputList);
+        /**
+         * Por qué Set? R/ Porque ofrece el método add que retorna
+         * si el elemento ya existía o no
+         *
+         */
         Set<Integer> set = new HashSet<>();
         int counter = 0;
+        /*
+        La funcion add del set retorna un booleano donde
+        true: Si el elemento se insertó correctamente, dado que no existía
+        false: contrario a lo anterior
+         */
         for (Integer i: inputList ) {
-            if(!set.add(i)) counter++;
+            if(!set.add(i)){
+                counter++;
+            }
         }
-        System.out.println(inputList);
-        System.out.println("Result => "+ counter );
+        System.out.println("example1 Result => "+ set );
 
-        //2)
-        List<String> strList = Arrays.asList("a,b,c,a,d,e,b,f,g".split(","));
-        System.out.println(strList);
+        System.out.println("example1 Result => "+ counter );
+        return counter;
+    }
+
+
+    /**
+     * Método que retorna un Map en donde el key es igual al elemento en la lista de entrada
+     * y el value es la cantidad de veces que se repite el mismo elemento en la
+     * lista de entrada
+     * @param strList
+     * @return
+     */
+    public static  Map<String, Integer> example2(  List<String> strList){
+        System.out.println("example2 Input  => "+strList);
+        /**
+         * POr que treeMao?
+         *
+         */
         Map<String, Integer> map = new TreeMap<>();
         for (String i: strList ) {
+            /*
+             Note como la operacio put devuelve null si el Key no existía
+             de lo contrario devuelve el valor del Key antes de ser actualizado
+             */
             if(map.put(i, 1)!=null) map.replace(i, map.get(i)+1);
         }
-        System.out.println(map);
+        System.out.println("example2 Result => "+map);
+        return map;
+    }
+
+
+    public static void main(String[] args) {
+        example1( Arrays.asList(10, 2, 10, 7, 5, 2, 9, 7, 8, 11,11, 12, 13, 14));
+        example2( Arrays.asList("a,b,c,a,d,e,b,f,g".split(",")));
+
     }
 }
