4027c34f285b3508a82488fd86b7658430a35c92
julianGonzalezV <j.andres2087@gmail.com>
2019-02-06 22:27:08 -0500

    prallel prog

--- chapters-study/src/main/java/co/book/chapter7/_7_concurrentCollections/ParallelClasses.java
+++ chapters-study/src/main/java/co/book/chapter7/_7_concurrentCollections/ParallelClasses.java
@@ -18,11 +18,35 @@ public class ParallelClasses {
 
 
     /**
-     * Creando stream paralelo desde una Java collection class
+     * Creando stream paralelo desde una Java collection class,usando parallelStream()
      *
      */
     public static Stream<Integer>  example2(){
         return Arrays.asList(1,2,3,4,5,6).parallelStream();
+    }
+
+
+
+    /**
+     * Processing Tasks in serial wayl
+     * @return la impresion en la consola en  orden
+     */
+    public static void example3(){
+
+        Stream st1 =  Arrays.asList(1,2,3,4,5,6).stream();
+        st1.forEach(s -> System.out.print(s+" "));
+        System.out.println("");
+    }
+
+    /**
+     * Processing Tasks in Parallel
+     * @return.
+     * Note comola impresion en la salida no siempre es en order
+     */
+    public static void example4(){
+
+        Stream st1 =  Arrays.asList(1,2,3,4,5,6).parallelStream();
+        st1.forEach(s -> System.out.print(s+" "));
     }
 
 
@@ -42,8 +66,13 @@ public class ParallelClasses {
         Stream<Integer> streamAfterConcat =  Stream.concat(example1(), example2());
         System.out.println(streamAfterConcat.isParallel());
 
-        Stream<Integer> streamAfterFlatMap = example1().fla(x -> x+1);
-        System.out.println(streamAfterFlatMap.isParallel());
+        /*Stream<Integer> streamAfterFlatMap = example1().fla(x -> x+1);
+        System.out.println(streamAfterFlatMap.isParallel());*/
+
+        example3();
+
+        System.out.println("example4 => ");
+        example4();
     }
 
 }
