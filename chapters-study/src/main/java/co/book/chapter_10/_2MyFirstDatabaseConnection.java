2430302199c2655e8c2a3d8b9cec31ad4c4aa0ae
julianGonzalezV <j.andres2087@gmail.com>
2019-02-26 22:31:58 -0500

    more jdbc

--- chapters-study/src/main/java/co/book/chapter_10/_2MyFirstDatabaseConnection.java
+++ chapters-study/src/main/java/co/book/chapter_10/_2MyFirstDatabaseConnection.java
@@ -1,6 +1,8 @@
 package co.book.chapter_10;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class _2MyFirstDatabaseConnection {
     /**
@@ -15,6 +17,15 @@ public class _2MyFirstDatabaseConnection {
              ResultSet rs = stmt.executeQuery("select name from animal")) {
             while (rs.next())
                 System.out.println(rs.getString(1));
+
+            Map<Integer, String> idToNameMap = new HashMap<>();
+            ResultSet rs2 = stmt.executeQuery("select id, name from species");
+            while(rs2.next()) {
+                int id = rs2.getInt("id");
+                String name = rs2.getString("name");
+                idToNameMap.put(id, name);
+            }
+            System.out.println(idToNameMap); // {1=African Elephant, 2=Zebra}
         }
     }
 }
